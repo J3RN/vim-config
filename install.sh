f11aa024f22eddedd6bd627e6c126e8dd20a7fbc
Jonathan Arnett <JArnett@covermymeds.com>
2015-06-02 14:16:35 -0400

    Made install more compatible

--- install.sh
+++ install.sh
@@ -1,14 +1,10 @@
-#! /bin/zsh
-
-green_print() {
-  echo "\e[1;32m$1\e[0m"
-}
+#! /bin/sh
 
 # Link .vim
 echo "Linking .vim directory..."
 
 if [ -e "$HOME/.vim" ]; then
-  green_print "Moving ~/.vim directory to ~/.vim.old"
+  echo "Moving ~/.vim directory to ~/.vim.old"
   mv -f "$HOME/.vim" "$HOME/.vim.old"
 fi
 
@@ -16,7 +12,7 @@ ln -si "$(pwd)/.vim" "$HOME"
 
 # Install Vundle
 if [ ! -e "$HOME/.vim/bundle/Vundle.vim" ]; then
-  green_print "Installing Vundle"
+  echo "Installing Vundle"
   git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
@@ -24,7 +20,7 @@ fi
 echo "Linking .vimrc..."
 
 if [ -e "$HOME/.vimrc" ]; then
-  green_print "Moving your ~/.vimrc to ~/.vimrc.old"
+  echo "Moving your ~/.vimrc to ~/.vimrc.old"
   mv -f "$HOME/.vimrc" "$HOME/.vimrc.old"
 fi
 
@@ -33,4 +29,4 @@ ln -si "$(pwd)/.vimrc" "$HOME"
 echo "Installing plugins..."
 vim +PluginInstall +qall
 
-green_print "All done! Enjoy!"
+echo "All done! Enjoy!"
